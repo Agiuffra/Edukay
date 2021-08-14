f07767be0b068c0bc86074a6a12bef1ff198a6e6
Alessandro <giuffra.alessandro@gmail.com>
2021-08-13 19:58:50 -0500

    avances

---
+++ data/proyectos.js
@@ -0,0 +1,134 @@
+const proyectos = [
+    {
+        nombre: "Potencia a tu profe",
+        descripcion: "El proyecto consiste en desarrollar talleres del uso de herramientas digitales a profesores con el objetivo de que ellos puedan mejorar el desarrollo de sus clases virtuales o incluso presenciales. Los talleres están enfocados en herramientas de conectividad y planificación, plataformas gratuitas para el desarrollo de las clases y herramientas de diseño y edición.",
+        aliados: [],
+        estudiantes: 0,
+        profesores: 221,
+        voluntarios: 24,
+        colegios: 0,
+        regiones: ["Lima", "Piura", "Huancavelica", "Ucayali", "Ancash", "Arequipa", "Lambayeque", "Huancayo", "Cajamarca", "Tacna", "Apurimac"],
+        culminado: true
+    },
+    {
+        nombre: "Descubre tu carrera STEAM",
+        descripcion: "Este proyecto busca generar  interes vocacionales en las carreras STEAM sujetas a las necesidades del mercado laboral actual. Para ello se dictan sesiones informativas dictadas por estudiantes universitarios de carreras STEAM.",
+        aliados: [],
+        estudiantes: 68,
+        profesores: 0,
+        voluntarios: 25,
+        colegios: 3,
+        regiones: ["Junín"],
+        culminado: true
+    },
+    {
+        nombre: "Ciencia pa' todos",
+        descripcion: "Este proyecto consiste en 3 sesiones experimentales de biotecnología dirigido a profesores de educación primaria. Mediante experimentos se busca que los profesores aprendan los conceptos de microorganismos, electrofóresis, extracción de ADN y la ciencia que existe en la cocina.",
+        aliados: ["BioUTEC"],
+        estudiantes: 0,
+        profesores: 29,
+        voluntarios: 6,
+        colegios: 0,
+        regiones: [],
+        culminado: true
+    },
+    {
+        nombre: "Construyendo mi propio mibot",
+        descripcion: "Este taller busca introducir a los estduiantes en el mundo de la robótica, explicando a los estudiantes las partes básicas que contiene un robot, así como las aplicaciones de un robot en la industria.",
+        aliados: ["GIIT Robotics"],
+        estudiantes: 15,
+        profesores: 0,
+        voluntarios: 2,
+        colegios: 1,
+        regiones: ["Junín"],
+        culminado: true
+    },
+    {
+        nombre: "SCRATCH EN TU COLE",
+        descripcion: "Este proyecto introduce a los estudiantes al mundo de la programación. Para ello, se utiliza la plataforma Scratch, en la cual se exploran los conceptos básicos de la programación que le permiten al estudiante crear su propio juego: ¡programándolo ellos mismos!",
+        aliados: ["GIIT Robotics"],
+        estudiantes: 15,
+        profesores: 0,
+        voluntarios: 3,
+        colegios: 1,
+        regiones: ["Junín"],
+        culminado: true
+    },
+    {
+        nombre: "KAMAY",
+        descripcion: "Est proyecto busca desarrollar el pensamiento compuacional de los estudiantes de zonas rurales mediante juegos lúdicos.",
+        aliados: ["RUN"],
+        estudiantes: 12,
+        profesores: 1,
+        voluntarios: 15,
+        colegios: 1,
+        regiones: ["Junín"],
+        culminado: true
+    },
+    {
+        nombre: "BOOTCAMP DE I&E",
+        descripcion: "Este proyecto introduce a los estudiantes de 4to y 5to de secundaria al mundo de la innovación y emprendimiento. Para ello se realizan talleres prácticos de Design Thinking, cómo contruir su Business Model Canvas, realizar un Pitch efectivo y se cierra con una presentación de los proyectos realizaod spos los estudiantes (Shark Tank).",
+        aliados: ["SPINOUT UTEC"],
+        estudiantes: 16,
+        profesores: 0,
+        voluntarios: 7,
+        colegios: 1,
+        regiones: ["Cajamarca"],
+        culminado: true
+    },
+    {
+        nombre: "Ciencia en casa",
+        descripcion: "Este proyecto consistió en dictar 3 talleres de ciencia que los estudiantes puedan realizar desde casa, de tal forma que al final de cada taller puedan aprendar más acerca de los biomateriales. En ese sentido, se busca incentivar el interés de los estudiantes en ciencia e ingeniería y sentar las bases sobre conocimientos en el área de biomateriales.",
+        aliados: ["Biomits - Ciencia Feliz"],
+        estudiantes: 24,
+        profesores: 0,
+        voluntarios: 5,
+        colegios: 2,
+        regiones: ["Lima", "Junín"],
+        culminado: true
+    },
+    {
+        nombre: "Taller Generador Eléctrico",
+        descripcion: "Este proyecto busca enseñar de forma práctica a estudiantes de secundaria cómo construir un generador eléctrico de bajo costo. Así como enseñar conceptos básicos de su funcionamiento.",
+        aliados: ["GIIT Robotics"],
+        estudiantes: 0,
+        profesores: 0,
+        voluntarios: 0,
+        colegios: 0,
+        regiones: [],
+        culminado: false
+    },
+    {
+        nombre: "Mentes creativas",
+        descripcion: "Este proyecto busca que los estudiantes de secundaria conozcan a fondo, sobre las carrera de artes escénicas, teatro, diseño gráfico, marketing y publicidad.",
+        aliados: ["Iniciativa edukayer"],
+        estudiantes: 0,
+        profesores: 0,
+        voluntarios: 0,
+        colegios: 0,
+        regiones: [],
+        culminado: false
+    },
+    {
+        nombre: "Iniciativa: Consciencia ambiental",
+        descripcion: "Este proyecto busca concientizar a los estudiantes acerca de los problemas ambientales del mundo. Además de ello generar interés en carreras STEAM y organizaciones que están alineadas directamente con dicha problemática.",
+        aliados: ["Iniciativa edukayer"],
+        estudiantes: 0,
+        profesores: 0,
+        voluntarios: 0,
+        colegios: 0,
+        regiones: [],
+        culminado: false
+    },
+    {
+        nombre: "Descubre tu carrera steam 2.0",
+        descripcion: "Este proyecto busca generar  interes vocacionales en las carreras STEAM sujetas a las necesidades del mercado laboral actual. Para ello se dictan sesiones informativas dictadas por estudiantes universitarios de carreras STEAM.",
+        aliados: [],
+        estudiantes: 0,
+        profesores: 0,
+        voluntarios: 0,
+        colegios: 0,
+        regiones: [],
+        culminado: false
+    },
+];
